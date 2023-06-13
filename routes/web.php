b950feb6122efe1f5b36c380a29c1c26a5199198
pasindu-dil <dilshan@mylinex.com>
2023-06-13 23:40:34 +0530

    Implement laravel passport authentication.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::get('/', function () {
     return view('welcome');
