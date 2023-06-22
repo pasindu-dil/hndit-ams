eb463663546deb4f760b4574a04a88027e48d3a2
pasindu-dil <dilshanpasindu357@gmail.com>
2023-06-22 21:57:12 +0530

    Implement assignment management api.

---
+++ database/migrations/2023_06_22_150637_create_assignments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assignments', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->nullable();
+            $table->text('description')->nullable();
+            $table->text('file')->nullable();
+            $table->integer('created_by');
+            $table->integer('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assignments');
+    }
+};
