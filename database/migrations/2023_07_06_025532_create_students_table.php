7ff98ded23a8cf60acf3c467418dcc6dadba1c91
pasindu-dil <dilshanpasindu357@gmail.com>
2023-07-06 08:42:27 +0530

    Implement student model and database migrations.

---
+++ database/migrations/2023_07_06_025532_create_students_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('reg_id', 50);
+            $table->string('nic', 20);
+            $table->string('name', 200);
+            $table->string('email', 200);
+            $table->text('address')->nullable();
+            $table->bigInteger('msisdn')->nullable();
+            $table->unsignedBigInteger('course_id');
+            $table->integer('created_by');
+            $table->integer('updated_by');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique(['reg_id', 'nic']);
+            $table->foreign('course_id')->references('id')->on('cources')->cascadeOnUpdate()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
